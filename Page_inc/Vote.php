314b9e4713d6e383e21b43bb758e5eb2717904f2
Lene <Lene@10.175.164.148>
2017-09-29 11:36:32 +0200

    Merge origin/L2 into NewL
    Conflicts:
    	index.php

--- Page_inc/Vote.php
+++ Page_inc/Vote.php
@@ -1,15 +1,5 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-// Author : Lene Tramm
-// Made on : 29-09-2017 @ 11:06:28
-
-
 $types = array("funny", "like", "dislike", "love");
 
 if(isset($_POST['photoId']) 
